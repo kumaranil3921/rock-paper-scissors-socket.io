d049accdb399a7842e5091a96036c1cb7a5aa966
Anil <>
2021-02-19 17:00:48 +0530

    web rtc 4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const peerServer = ExpressPeerServer(http, {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(peerServer);
+app.use('/peerjs', peerServer);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/media', express.static(path.join(__dirname, 'media')));
 app.use(bodyParser.urlencoded({ extended: true }));
