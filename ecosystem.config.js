d02c6aee15a5c913c8e20c2bffd2f8ce8daba0a7
Anil <>
2021-02-19 11:33:08 +0530

    FIX: install pm2#1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    instances: 'max',
+    instances: 1,
     autorestart: true,
     env: {
       NODE_ENV: 'development'
